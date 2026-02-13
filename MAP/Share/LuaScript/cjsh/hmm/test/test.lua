7d2210e7625b7750cb0289d48c792f0e33ed009c
lovegnep <1256136108@qq.com>
2026-02-13 13:49:37 +0800

    增加测试脚本

---
+++ MAP/Share/LuaScript/cjsh/hmm/test/test.lua
@@ -0,0 +1,337 @@
+
+--DECLARE_QUEST_INFO_START--
+
+x930212_var_FileId = 930212   
+
+x930212_var_SelectIndex = {}
+
+-- 兑换目标道具ID
+x930212_var_ExcObjID = {
+    {index=1,level=30,buffID=2240,buffDesc="经验获得增加1%"},
+    {index=2,level=61,buffID=2241,buffDesc="经验获得增加2%"},
+    {index=3,level=91,buffID=2242,buffDesc="经验获得增加3%"},
+    {index=4,level=122,buffID=2243,buffDesc="经验获得增加4%"},
+    {index=5,level=152,buffID=2244,buffDesc="经验获得增加5%"},
+    {index=6,level=183,buffID=2245,buffDesc="经验获得增加6%"},
+    {index=7,level=213,buffID=2246,buffDesc="经验获得增加7%"},
+    {index=8,level=244,buffID=2247,buffDesc="经验获得增加8%"},
+    {index=9,level=274,buffID=2248,buffDesc="经验获得增加9%"},
+    {index=10,level=305,buffID=2249,buffDesc="经验获得增加10%"},
+    {index=11,level=335,buffID=2250,buffDesc="经验获得增加11%"},
+    {index=12,level=366,buffID=2251,buffDesc="经验获得增加12%"},
+    {index=13,level=396,buffID=2252,buffDesc="经验获得增加13%"},
+    {index=14,level=427,buffID=2253,buffDesc="经验获得增加14%"},
+    {index=15,level=457,buffID=2254,buffDesc="经验获得增加15%"},
+    {index=16,level=488,buffID=2255,buffDesc="经验获得增加16%"},
+    {index=17,level=518,buffID=2256,buffDesc="经验获得增加17%"},
+    {index=18,level=549,buffID=2257,buffDesc="经验获得增加18%"},
+    {index=19,level=579,buffID=2258,buffDesc="经验获得增加19%"},
+    {index=20,level=610,buffID=2259,buffDesc="经验获得增加20%"},
+    {index=21,level=640,buffID=2260,buffDesc="经验获得增加21%"},
+    {index=22,level=671,buffID=2261,buffDesc="经验获得增加22%"},
+    {index=23,level=701,buffID=2262,buffDesc="经验获得增加23%"},
+    {index=24,level=732,buffID=2263,buffDesc="经验获得增加24%"},
+    {index=25,level=762,buffID=2264,buffDesc="经验获得增加25%"},
+    {index=26,level=793,buffID=2265,buffDesc="经验获得增加26%"},
+    {index=27,level=823,buffID=2266,buffDesc="经验获得增加27%"},
+    {index=28,level=854,buffID=2267,buffDesc="经验获得增加28%"},
+    {index=29,level=884,buffID=2268,buffDesc="经验获得增加29%"},
+    {index=30,level=915,buffID=2269,buffDesc="经验获得增加30%"},
+    {index=31,level=945,buffID=2270,buffDesc="经验获得增加31%"},
+    {index=32,level=976,buffID=2271,buffDesc="经验获得增加32%"},
+    {index=33,level=1006,buffID=2272,buffDesc="经验获得增加33%"},
+    {index=34,level=1037,buffID=2273,buffDesc="经验获得增加34%"},
+    {index=35,level=1067,buffID=2274,buffDesc="经验获得增加35%"},
+    {index=36,level=1098,buffID=2275,buffDesc="经验获得增加36%"},
+    {index=37,level=1128,buffID=2276,buffDesc="经验获得增加37%"},
+    {index=38,level=1159,buffID=2277,buffDesc="经验获得增加38%"},
+    {index=39,level=1189,buffID=2278,buffDesc="经验获得增加39%"},
+    {index=40,level=1220,buffID=2279,buffDesc="经验获得增加40%"},
+    {index=41,level=1250,buffID=2280,buffDesc="经验获得增加41%"},
+    {index=42,level=1281,buffID=2281,buffDesc="经验获得增加42%"},
+    {index=43,level=1311,buffID=2282,buffDesc="经验获得增加43%"},
+    {index=44,level=1342,buffID=2283,buffDesc="经验获得增加44%"},
+    {index=45,level=1372,buffID=2284,buffDesc="经验获得增加45%"},
+    {index=46,level=1403,buffID=2285,buffDesc="经验获得增加46%"},
+    {index=47,level=1433,buffID=2286,buffDesc="经验获得增加47%"},
+    {index=48,level=1464,buffID=2287,buffDesc="经验获得增加48%"},
+    {index=49,level=1494,buffID=2288,buffDesc="经验获得增加49%"},
+    {index=50,level=1525,buffID=2289,buffDesc="经验获得增加50%"},
+    {index=51,level=1555,buffID=2290,buffDesc="经验获得增加51%"},
+    {index=52,level=1586,buffID=2291,buffDesc="经验获得增加52%"},
+    {index=53,level=1616,buffID=2292,buffDesc="经验获得增加53%"},
+    {index=54,level=1647,buffID=2293,buffDesc="经验获得增加54%"},
+    {index=55,level=1677,buffID=2294,buffDesc="经验获得增加55%"},
+    {index=56,level=1708,buffID=2295,buffDesc="经验获得增加56%"},
+    {index=57,level=1738,buffID=2296,buffDesc="经验获得增加57%"},
+    {index=58,level=1769,buffID=2297,buffDesc="经验获得增加58%"},
+    {index=59,level=1799,buffID=2298,buffDesc="经验获得增加59%"},
+    {index=60,level=1830,buffID=2299,buffDesc="经验获得增加60%"},
+    {index=61,level=1860,buffID=2300,buffDesc="经验获得增加61%"},
+    {index=62,level=1891,buffID=2301,buffDesc="经验获得增加62%"},
+    {index=63,level=1921,buffID=2302,buffDesc="经验获得增加63%"},
+    {index=64,level=1952,buffID=2303,buffDesc="经验获得增加64%"},
+    {index=65,level=1982,buffID=2304,buffDesc="经验获得增加65%"},
+    {index=66,level=2013,buffID=2305,buffDesc="经验获得增加66%"},
+    {index=67,level=2043,buffID=2306,buffDesc="经验获得增加67%"},
+    {index=68,level=2074,buffID=2307,buffDesc="经验获得增加68%"},
+    {index=69,level=2104,buffID=2308,buffDesc="经验获得增加69%"},
+    {index=70,level=2135,buffID=2309,buffDesc="经验获得增加70%"},
+    {index=71,level=2165,buffID=2310,buffDesc="经验获得增加71%"},
+    {index=72,level=2196,buffID=2311,buffDesc="经验获得增加72%"},
+    {index=73,level=2226,buffID=2312,buffDesc="经验获得增加73%"},
+    {index=74,level=2257,buffID=2313,buffDesc="经验获得增加74%"},
+    {index=75,level=2287,buffID=2314,buffDesc="经验获得增加75%"},
+    {index=76,level=2318,buffID=2315,buffDesc="经验获得增加76%"},
+    {index=77,level=2348,buffID=2316,buffDesc="经验获得增加77%"},
+    {index=78,level=2379,buffID=2317,buffDesc="经验获得增加78%"},
+    {index=79,level=2409,buffID=2318,buffDesc="经验获得增加79%"},
+    {index=80,level=2440,buffID=2319,buffDesc="经验获得增加80%"},
+    {index=81,level=2470,buffID=2320,buffDesc="经验获得增加81%"},
+    {index=82,level=2501,buffID=2321,buffDesc="经验获得增加82%"},
+    {index=83,level=2531,buffID=2322,buffDesc="经验获得增加83%"},
+    {index=84,level=2562,buffID=2323,buffDesc="经验获得增加84%"},
+    {index=85,level=2592,buffID=2324,buffDesc="经验获得增加85%"},
+    {index=86,level=2623,buffID=2325,buffDesc="经验获得增加86%"},
+    {index=87,level=2653,buffID=2326,buffDesc="经验获得增加87%"},
+    {index=88,level=2684,buffID=2327,buffDesc="经验获得增加88%"},
+    {index=89,level=2714,buffID=2328,buffDesc="经验获得增加89%"},
+    {index=90,level=2745,buffID=2329,buffDesc="经验获得增加90%"},
+    {index=91,level=2775,buffID=2330,buffDesc="经验获得增加91%"},
+    {index=92,level=2806,buffID=2331,buffDesc="经验获得增加92%"},
+    {index=93,level=2836,buffID=2332,buffDesc="经验获得增加93%"},
+    {index=94,level=2867,buffID=2333,buffDesc="经验获得增加94%"},
+    {index=95,level=2897,buffID=2334,buffDesc="经验获得增加95%"},
+    {index=96,level=2928,buffID=2335,buffDesc="经验获得增加96%"},
+    {index=97,level=2958,buffID=2336,buffDesc="经验获得增加97%"},
+    {index=98,level=2989,buffID=2337,buffDesc="经验获得增加98%"},
+    {index=99,level=3019,buffID=2338,buffDesc="经验获得增加99%"},
+    {index=100,level=3050,buffID=2339,buffDesc="经验获得增加100%"},
+
+}
+
+
+x930212_var_QuestName  = "【升级】#R悟性"
+--最小等级限制
+x930212_var_minlevel = 30
+
+
+
+MD_TEST320 = {320,0,32}
+
+function x930212_ProcEventEntry( varMap, varPlayer, varTalknpc ,varScript,seleteId)
+    local old = x930212_GetRefreshTimes(varMap, varPlayer)
+    if seleteId == -1 then--增加
+
+        x930212_SetRefreshTimes(varMap, varPlayer, old+1)
+    elseif seleteId == 0 then
+        x930212_SetRefreshTimes(varMap, varPlayer, old-1)
+    elseif seleteId == 1 then
+        --啥也不做
+    end
+    local now = x930212_GetRefreshTimes(varMap, varPlayer)
+    Msg2Player(varMap, varPlayer,format("当前值：%d/%d", old,now),8,3)
+    Msg2Player(varMap, varPlayer,format("当前值：%d/%d", old,now),8,2)
+end
+
+function x930212_ProcEnumEvent( varMap, varPlayer, varTalknpc, varQuest )
+    TalkAppendButton(varMap,x930212_var_FileId,"增加",3,-1);
+    TalkAppendButton(varMap,x930212_var_FileId,"减少",3,0);
+    TalkAppendButton(varMap,x930212_var_FileId,"查询",3,1);
+end
+
+function x930212_ProcAccept( varMap, varPlayer )
+
+	x930212_Accept( varMap, varPlayer )
+	
+end
+
+function x930212_Accept( varMap, varPlayer )
+
+
+    --查询当前buffid
+    local curIndex = 0
+    for varI, item in x930212_var_ExcObjID do
+        local haveImpact  = IsHaveSpecificImpact(varMap, varPlayer, item.buffID)
+        if haveImpact > 0 then
+            curIndex = varI
+        end
+    end
+
+    local nextIndex = curIndex + 1
+    if nextIndex > getn(x930212_var_ExcObjID) then
+        Msg2Player(varMap, varPlayer,"您已经达到最大效果了！",8,3)
+        Msg2Player(varMap, varPlayer,"您已经达到最大效果了！",8,2)
+        return
+    end
+
+    local levelLimit = x930212_var_minlevel
+    if GetLevel( varMap, varPlayer) < levelLimit then
+        Msg2Player(varMap, varPlayer,format("人物等级需要达到%d", levelLimit),8,3)
+        Msg2Player(varMap, varPlayer,format("人物等级需要达到%d", levelLimit),8,2)
+        return
+    end
+
+
+    local curJY = x930212_GetRefreshTimes(varMap,varPlayer)
+    if curJY < x930212_var_ExcObjID[nextIndex].level then
+        Msg2Player(varMap, varPlayer,format("经验不够，请继续肝吧"),8,3)
+        Msg2Player(varMap, varPlayer,format("经验不够，请继续肝吧"),8,2)
+        return
+    end
+
+    if curIndex > 0 then
+        local varRet = CancelSpecificImpact( varMap, varPlayer, x930212_var_ExcObjID[curIndex].buffID)
+        if varRet <= 0 then
+            Msg2Player(varMap, varPlayer,"删除效果"..x930212_var_ExcObjID[curIndex].buffID.."失败",8,3)
+            Msg2Player(varMap, varPlayer,"删除效果"..x930212_var_ExcObjID[curIndex].buffID.."失败",8,2)
+            return
+        else
+            Msg2Player(varMap, varPlayer,"删除效果"..x930212_var_ExcObjID[curIndex].buffID.."成功",8,3)
+            Msg2Player(varMap, varPlayer,"删除效果"..x930212_var_ExcObjID[curIndex].buffID.."成功",8,2)
+        end
+    end
+    SendSpecificImpactToUnit(varMap, varPlayer, varPlayer, varPlayer, x930212_var_ExcObjID[nextIndex].buffID, 0);
+    Msg2Player(varMap, varPlayer,"添加效果"..x930212_var_ExcObjID[nextIndex].buffID.."成功",8,3)
+    Msg2Player(varMap, varPlayer,"添加效果"..x930212_var_ExcObjID[nextIndex].buffID.."成功",8,2)
+
+    x930212_SetRefreshTimes(varMap, varPlayer, 0)
+    local name = GetName(varMap,varPlayer)
+    local varMsg = format("恭喜玩家#R%s#W悟性等级提升到#R%d#W级", name, nextIndex)
+    LuaAllScenceM2Wrold(varMap, varMsg, CHAT_PLANE_SCROLL, 1)
+    LuaAllScenceM2Wrold(varMap, varMsg, CHAT_LEFTDOWN, 1)
+    LuaAllScenceM2Wrold(varMap, varMsg, CHAT_MAIN_RIGHTDOWN, 1)
+end
+		
+function x930212_ProcAcceptCheck( varMap, varPlayer, varTalknpc )
+	return 1
+
+end
+
+
+
+function x930212_ProcCommitPet( varMap, varPlayer, varQuest, varTalknpc, horseGUID_high, horseGUID_low, varIndex)
+
+end
+
+
+function x930212_ProcQuestAbandon( varMap, varPlayer, varQuest )
+
+end
+
+
+
+
+function x930212_ProcContinue( varMap, varPlayer, varTalknpc )
+
+end
+
+
+
+
+function x930212_CheckSubmit( varMap, varPlayer )
+
+end
+
+
+
+
+function x930212_ProcQuestSubmit( varMap, varPlayer, varTalknpc,varRadioSelected, varQuest )
+
+end
+
+
+
+
+function x930212_ProcQuestObjectKilled( varMap, varPlayer, varObjData, varObj, varQuest )
+
+end
+
+
+
+
+function x930212_ProcAreaEntered( varMap, varPlayer, varArea, varQuest )
+end
+
+
+
+
+function x930212_ProcQuestItemChanged( varMap, varPlayer, varItemData, varQuest )
+end
+
+
+function x930212_Upwaronleveladd(varMap, varPlayer, change)
+    if change <= 0 then
+        return
+    end
+    local warlevel = GetPlayerGameData(varMap, varPlayer, MD_GETBUFF_DAY1[1], MD_GETBUFF_DAY1[2], MD_GETBUFF_DAY1[3])
+
+    local curLevel = 0
+    local nextLevel = 0
+    for vari, item in x930212_var_ExcObjID do
+        if item.level >= warlevel then
+            curLevel = vari
+        end
+        if item.level >= warlevel+change then
+            nextLevel = vari
+        end
+    end
+
+    --保存最新
+    SetPlayerGameData(varMap, varPlayer, MD_GETBUFF_DAY1[1], MD_GETBUFF_DAY1[2], MD_GETBUFF_DAY1[3],warlevel+change)
+
+
+    if curLevel == nextLevel then
+        --没有任何变化
+        return
+    end
+
+
+    if curLevel > 0 then
+        local varRet = CancelSpecificImpact( varMap, varPlayer, x930212_var_ExcObjID[curLevel].buffID)
+        if varRet <= 0 then
+            Msg2Player(varMap, varPlayer,"删除效果"..x930212_var_ExcObjID[curLevel].buffID.."失败",8,3)
+            Msg2Player(varMap, varPlayer,"删除效果"..x930212_var_ExcObjID[curLevel].buffID.."失败",8,2)
+        else
+            Msg2Player(varMap, varPlayer,"删除效果"..x930212_var_ExcObjID[curLevel].buffID.."成功",8,3)
+            Msg2Player(varMap, varPlayer,"删除效果"..x930212_var_ExcObjID[curLevel].buffID.."成功",8,2)
+        end
+    end
+    SendSpecificImpactToUnit(varMap, varPlayer, varPlayer, varPlayer, x930212_var_ExcObjID[nextLevel].buffID, 0);
+    Msg2Player(varMap, varPlayer,"添加效果"..x930212_var_ExcObjID[nextLevel].buffID.."成功",8,3)
+    Msg2Player(varMap, varPlayer,"添加效果"..x930212_var_ExcObjID[nextLevel].buffID.."成功",8,2)
+
+    local name = GetName(varMap,varPlayer)
+    local varMsg = format("恭喜玩家#R%s#W战神降临buff等级升级到#R%d#W级", name, nextLevel)
+    LuaAllScenceM2Wrold(varMap, varMsg, CHAT_PLANE_SCROLL, 1)
+    LuaAllScenceM2Wrold(varMap, varMsg, CHAT_LEFTDOWN, 1)
+    LuaAllScenceM2Wrold(varMap, varMsg, CHAT_MAIN_RIGHTDOWN, 1)
+
+end
+function x930212_Upwaronleveldec(varMap, varPlayer, change)
+
+end
+
+
+
+-- 获取刷新次数
+function x930212_GetRefreshTimes(varMap, varPlayer)
+    return GetPlayerGameData(varMap, varPlayer, MD_TEST320[1], MD_TEST320[2], MD_TEST320[3])
+end
+
+-- 设置刷新次数
+function x930212_SetRefreshTimes(varMap, varPlayer, times)
+    SetPlayerGameData(varMap, varPlayer, MD_TEST320[1], MD_TEST320[2], MD_TEST320[3], times)
+end
+
+
+
+function x930212_MsgToAllPlayers(varMap, strMsg)
+    local humancount = GetFuben_PlayerCount(varMap)
+    for i = 0, humancount - 1 do
+        local humanId = GetFuben_PlayerObjId(varMap, i)
+        if humanId ~= nil and humanId >= 0 then
+            Msg2Player(varMap, humanId, strMsg, 8, 2)
+        end
+    end
+end
